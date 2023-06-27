c85c71412070dc52b580a5e7895b40e66ef9d871
angelyunes1 <angelyunes1@gmail.com>
2023-06-26 21:25:29 -0300

    intento 3

---
+++ Python/Proyecto/intento3/conexionInterface.py
@@ -0,0 +1,70 @@
+import psycopg2 # Importamos la base de datos, usaremos PostgresSQL
+class ConexionDB:#Definimos la clase
+    # Establecemos la conexion con la base de datos
+    def __init__(self):
+        self.conexion = psycopg2.connect(
+            host="localhost",
+            database="proyecto_Python",
+            user="postgres",
+            password="admin"
+        )
+        self.crear_tabla()# LLamamos al metodo crear tabla
+
+    def crear_tabla(self):
+        # Con este metodo creamos la tabla 'deseos' si no existe
+        # Utilizara los siguientes campos: id, producto, cantidad, precio
+        cursor = self.conexion.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS deseos (
+                id SERIAL PRIMARY KEY,
+                producto TEXT,
+                cantidad INTEGER,
+                precio REAL
+            )
+            """
+        )
+        self.conexion.commit()
+
+    def agregar_deseo(self, producto, cantidad, precio):
+        # Insertamos los datos en la tabla 'deseos'
+        cursor = self.conexion.cursor()
+        cursor.execute(
+            """
+            INSERT INTO deseos (producto, cantidad, precio)
+            VALUES (%s, %s, %s)
+            """,
+            (producto, cantidad, precio)
+        )
+        self.conexion.commit()
+
+    def eliminar_deseo(self, id_deseo):
+        # Eliminamos el registro con el ID proporcionado de la tabla 'deseos'
+        cursor = self.conexion.cursor()
+        cursor.execute(
+            """
+            DELETE FROM deseos WHERE id = %s
+            """,
+            (id_deseo,)
+        )
+        self.conexion.commit()
+
+    def eliminar_todo(self):
+        # Eliminamos todos los registros de la tabla 'deseos'
+        cursor = self.conexion.cursor()
+        cursor.execute(
+            """
+            DELETE FROM deseos
+            """
+        )
+        self.conexion.commit()
+
+    def obtener_deseos(self):
+        # Obtenemos todos los registros de la tabla 'deseos'
+        cursor = self.conexion.cursor()
+        cursor.execute(
+            """
+            SELECT * FROM deseos
+            """
+        )
+        return cursor.fetchall()
---
+++ Python/Proyecto/intento3/interfaz.py
@@ -0,0 +1,102 @@
+import tkinter as tk
+from conexionInterface import ConexionDB
+from tkinter import messagebox
+class InterfazListaDeseos:
+    def __init__(self, conexion):
+        self.conexion = conexion
+
+        self.ventana = tk.Tk()
+        self.ventana.title("Lista de Deseos")
+        self.ventana.configure(bg="black")  # Configurar el color de fondo para tema oscuro
+
+        self.crear_interfaz()# Creamos la interfaz gráfica
+
+    def crear_interfaz(self):
+        # Crear los elementos de la interfaz
+        # Título de la Interfaz
+        self.titulo_label = tk.Label(self.ventana, text="Lista de Deseos Grupo GIGABYTE", font=("Arial", 16), bg="black", fg="white")
+        self.titulo_label.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
+        # Etiqueta y campo de texto para ingresar el producto
+        self.producto_label = tk.Label(self.ventana, text="Producto:", bg="black", fg="white")
+        self.producto_label.grid(row=1, column=0, padx=5, pady=5, sticky="e")
+
+        self.producto_entry = tk.Entry(self.ventana, bg="black", fg="white")
+        self.producto_entry.grid(row=1, column=1, padx=5, pady=5)
+        # Etiqueta y campo de texto para ingresar la cantidad
+        self.cantidad_label = tk.Label(self.ventana, text="Cantidad:", bg="black", fg="white")
+        self.cantidad_label.grid(row=2, column=0, padx=5, pady=5, sticky="e")
+
+        self.cantidad_entry = tk.Entry(self.ventana, bg="black", fg="white")
+        self.cantidad_entry.grid(row=2, column=1, padx=5, pady=5)
+        # Etiqueta y campo de texto para ingresar el precio
+        self.precio_label = tk.Label(self.ventana, text="Precio:", bg="black", fg="white")
+        self.precio_label.grid(row=3, column=0, padx=5, pady=5, sticky="e")
+
+        self.precio_entry = tk.Entry(self.ventana, bg="black", fg="white")
+        self.precio_entry.grid(row=3, column=1, padx=5, pady=5)
+        # Botón para agregar un deseo
+        self.agregar_button = tk.Button(self.ventana, text="Agregar", command=self.agregar_deseo, bg="black", fg="white")
+        self.agregar_button.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
+        # Campo de texto para mostrar la lista de deseos
+        self.eliminar_label = tk.Label(self.ventana, text="ID Deseo a eliminar:", bg="black", fg="white")
+        self.eliminar_label.grid(row=5, column=0, padx=5, pady=5, sticky="e")
+
+        self.eliminar_entry = tk.Entry(self.ventana, bg="black", fg="white")
+        self.eliminar_entry.grid(row=5, column=1, padx=5, pady=5)
+        # Botón para eliminar un deseo por ID
+        self.eliminar_button = tk.Button(self.ventana, text="Eliminar por ID", command=self.eliminar_deseo, bg="black", fg="white")
+        self.eliminar_button.grid(row=6, column=0, columnspan=2, padx=5, pady=5)
+        # Botón para eliminar todos los deseos
+        self.eliminar_todo_button = tk.Button(self.ventana, text="Eliminar Todo", command=self.eliminar_todo, bg="black", fg="white")
+        self.eliminar_todo_button.grid(row=7, column=0, columnspan=2, padx=5, pady=5)
+
+        self.lista_deseos_text = tk.Text(self.ventana, bg="black", fg="white")  # Configurar colores para tema oscuro
+        self.lista_deseos_text.grid(row=8, column=0, columnspan=2, padx=5, pady=5)
+
+        self.actualizar_lista_deseos()  # Mostrar la lista de deseos al iniciar la interfaz
+
+    def actualizar_lista_deseos(self):
+        deseos = self.conexion.obtener_deseos()
+
+        self.lista_deseos_text.delete("1.0", tk.END)  # Limpiar el campo de texto
+
+        if deseos:
+            for deseo in deseos:
+                texto = f"ID: {deseo[0]}, Producto: {deseo[1]}, Cantidad: {deseo[2]}, Precio: {deseo[3]}\n"
+                self.lista_deseos_text.insert(tk.END, texto)
+        else:
+            self.lista_deseos_text.insert(tk.END, "No hay elementos en la lista de deseos.")
+
+    def agregar_deseo(self):
+        # Código para agregar un deseo a la base de datos
+        producto = self.producto_entry.get()
+        cantidad = int(self.cantidad_entry.get())
+        precio = float(self.precio_entry.get())
+
+        self.conexion.agregar_deseo(producto, cantidad, precio)
+        print("Deseo agregado exitosamente.")
+
+        self.actualizar_lista_deseos()  # Actualizar la lista mostrada en la interfaz
+
+    def eliminar_deseo(self):
+        # Código para eliminar un deseo de la base de datos por su ID
+        id_deseo = self.eliminar_entry.get()
+        if id_deseo:
+            self.conexion.eliminar_deseo(id_deseo)
+            print("Deseo eliminado exitosamente.")
+
+            self.actualizar_lista_deseos()  # Actualizar la lista mostrada en la interfaz
+        else:
+            print("No se ha ingresado un ID de deseo.")
+
+    def eliminar_todo(self):
+        # Código para eliminar todos los deseos de la base de datos
+        confirmacion = tk.messagebox.askquestion("Eliminar Todo", "¿Estás seguro de que deseas eliminar todos los deseos?")
+        if confirmacion == "yes":
+            self.conexion.eliminar_todo()
+            print("Todos los deseos han sido eliminados.")
+
+            self.actualizar_lista_deseos()  # Actualizar la lista mostrada en la interfaz
+
+    def iniciar(self):
+        self.ventana.mainloop()
